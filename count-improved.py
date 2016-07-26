47e53d67f760830dfb1b5d811ad83e4b5146f9a1
jasleenkaur <jasleen.7956@gmail.com>
2016-07-27 01:17:45 +0530

    double contour-iprove algo

--- count-improved.py
+++ count-improved.py
@@ -20,18 +20,25 @@ gray_hsv = cv2.cvtColor(hsv,cv2.COLOR_BGR2GRAY)
 ret, thresh_hsv = cv2.threshold(gray_hsv,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 # noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh_hsv,cv2.MORPH_OPEN,kernel, iterations = 1)
+kernel = np.ones((5,5),np.uint8)
 
+opening = cv2.morphologyEx(thresh_hsv,cv2.MORPH_OPEN,kernel, iterations = 2)
+#erosion = cv2.erode(thresh_hsv,kernel,iterations = 1)
+#cv2.imshow("erosion",erosion)
+cv2.imwrite("opening.jpg",opening)
 _, contours, hierarchy= cv2.findContours(opening, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    cv2.drawContours(opening,[cnt],0,255,-1)
+
+_, contours1, hierarchy= cv2.findContours(opening, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 idx =0
 count = 0 
-for cnt in contours:
+for cnt in contours1:
     cv2.drawContours(opening,[cnt],0,255,-1)
     idx += 1
     area = cv2.contourArea(cnt)
     print str(idx) +"="+ str(area)
-    if area <20:  # percentile should be here
+    if area <30:  # percentile should be here
         continue
     x,y,w,h = cv2.boundingRect(cnt)
     count=count+1
@@ -45,14 +52,16 @@ print "total number of bricks: " +  str(idx) +"  count  =" +str(count)
 
 t2 = time.time() -t1
 print "time taken(seconds): " + str(t2)
+'''
 plt.hist(gray_hsv.ravel(),256,[0,256]); plt.show()
+
 titles = ['Original Image', 'hsv image', 'Thresholding',
-            'opening' ]
-images = [img, hsv, thresh_hsv, opening]
-for i in xrange(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+            'erosion','opening' ]
+images = [img, hsv, thresh_hsv, erosion,opening]
+for i in xrange(5):
+    plt.subplot(3,2,i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 plt.show()
-
+'''
 cv2.waitKey(0)
