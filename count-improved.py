94793d524ff823ccf9dbcd6d0b87e12b30ba9671
jasleenkaur <jasleen.7956@gmail.com>
2016-07-27 04:45:57 +0530

    improved

--- count-improved.py
+++ count-improved.py
@@ -20,15 +20,17 @@ gray_hsv = cv2.cvtColor(hsv,cv2.COLOR_BGR2GRAY)
 ret, thresh_hsv = cv2.threshold(gray_hsv,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 # noise removal
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((3,3),np.uint8)
 
 opening = cv2.morphologyEx(thresh_hsv,cv2.MORPH_OPEN,kernel, iterations = 2)
 #erosion = cv2.erode(thresh_hsv,kernel,iterations = 1)
 #cv2.imshow("erosion",erosion)
-cv2.imwrite("opening.jpg",opening)
+
+# to fill holes
 _, contours, hierarchy= cv2.findContours(opening, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 for cnt in contours:
     cv2.drawContours(opening,[cnt],0,255,-1)
+cv2.imwrite("opening.jpg",opening)
 
 _, contours1, hierarchy= cv2.findContours(opening, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 idx =0
@@ -41,8 +43,10 @@ for cnt in contours1:
     if area <30:  # percentile should be here
         continue
     x,y,w,h = cv2.boundingRect(cnt)
+    cv2.drawContours( img, contours,-1, (0,255,0),2) 
     count=count+1
 print "total number of bricks: " +  str(idx) +"  count  =" +str(count)
+cv2.imwrite("img.jpg",img)
     #idx += 1
     #x,y,w,h = cv2.boundingRect(cnt)
     #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
